e9857e22de7369d13e9f1e1bd566f5e2b9dec0f4
k.sathurjan415@gmail.com <k.sathurjan415@gmail.com>
2020-08-31 05:51:15 +0530

    funwork

---
+++ main.js
@@ -0,0 +1,268 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+  $(document).ready(function(){
+   
+    SC.stream(' /tracks/362416091',function(sound){
+        //start button function
+        id=song1
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream(' /tracks/307879985',function(sound){
+        //start button function
+        id=song2
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream('/tracks/711638860',function(sound){
+        //start button function
+        id=song3
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     SC.stream(' /tracks/668644226',function(sound){
+        //start button function
+        id=song4
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream('/tracks/582068346',function(sound){
+        //start button function
+        id=song5
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream(' /tracks/571200702',function(sound){
+        //start button function
+        id=song6
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream('/tracks/557856666',function(sound){
+        //start button function
+        id=song7
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream('/tracks/540186672',function(sound){
+        //start button function
+        id=song8
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+   
+     SC.stream('/tracks/524553387',function(sound){
+        //start button function
+        id=song9
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+     
+     SC.stream(" /tracks/500364810",function(sound){
+        //start button function
+        id=song10
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+            
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+    
+     
+     SC.stream( '/tracks/470135838',function(sound){
+        //start button function
+        id=song11
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+    
+     SC.stream('/tracks/354663899',function(sound){
+        //start button function
+        id=song12
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+    
+     SC.stream('/tracks/318305340',function(sound){
+        //start button function
+        id=song13
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+     SC.stream('/tracks/318305340',function(sound){
+        //start button function
+        id=song14
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+     SC.stream('/tracks/318305340',function(sound){
+        //start button function
+        id=song15
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+     SC.stream('/tracks/319248825',function(sound){
+        //start button function
+        id=song16
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          //stop button function
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+  });
